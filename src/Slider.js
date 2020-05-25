205858db2bba4a94b8560c3d8f799c7d7654fb09
Ivan <ivan1501@gmail.com>
2020-05-25 15:31:40 +0200

    Added slideing option between pictures and style.

--- src/Slider.js
+++ src/Slider.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './slider.scss';
 
 function Slider() {
 
     //Array for show component inside slider
     let sliderArray = [1,2,3,4,5];
-
+    const [x, setX] = useState(0);
+    const goLeft=() => {
+        console.log(x);
+        setX(x + 100);
+    };
+    const goRight=() => {
+        console.log(x);
+        setX(x - 100);
+    };
     return (
         <div className="slider">
             {sliderArray.map((item, index) => {
                     return (
-                        <div key={index} className="slide">
+                        <div key={index} className="slide" style={{transform:`translateX(${x}%)`}}>
                             {item}
                         </div>
                     );
             })}
+            <button id="goLeft" onClick={goLeft}>Left</button>
+            <button id="goRight" onClick={goRight}>Right</button>
         </div>
     );
 }
