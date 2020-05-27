d7cec457f10ce4625838aac12f062f292eaaebe0
Ivan <ivan1501@gmail.com>
2020-05-27 08:27:58 +0200

    Added text and working on style.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Slider from './Slider';
+import Text from './Text';
 
 function App() {
   return (
     <div className="App">
       <Slider />
+      <Text />
     </div>
   );
 }
---
+++ src/Text.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Text extends Component {
+    
+    render() {
+
+        return (
+            <div className="text">
+            <p><h2>Lorem ipsum dolor sit amet consectetur adipiscing elit</h2></p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            </div>
+            )
+        }
+    }
+    
+export default Text
